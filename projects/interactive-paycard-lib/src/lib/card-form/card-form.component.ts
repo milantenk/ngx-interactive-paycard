361a934c7dc4537713ec900156701dd629cb3460
Milan Tenk <tenkmilan@gmail.com>
2020-01-26 12:39:57 +0100

    Card form scss added

--- projects/interactive-paycard-lib/src/lib/card-form/card-form.component.ts
+++ projects/interactive-paycard-lib/src/lib/card-form/card-form.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'card-form',
-    templateUrl: 'card-form.component.html'
+    templateUrl: 'card-form.component.html',
+    styleUrls: ['./card-form.component.scss']
 })
 
 export class CardFormComponent implements OnInit {
--- projects/interactive-paycard-lib/src/lib/card/card.component.ts
+++ projects/interactive-paycard-lib/src/lib/card/card.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'card',
-    templateUrl: 'card.component.html'
+    templateUrl: 'card.component.html' 
 })
 
 export class CardComponent implements OnInit {
