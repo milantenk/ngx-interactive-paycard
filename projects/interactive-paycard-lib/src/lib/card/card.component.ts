b704a9d0ba2da4d9842d26062583732714cfa710
Milan Tenk <tenkmilan@gmail.com>
2020-04-19 12:31:25 +0200

    Card - Some Cleaning done

--- projects/interactive-paycard-lib/src/lib/card/card.component.ts
+++ projects/interactive-paycard-lib/src/lib/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CardModel } from '../shared/card-model';
 
 @Component({
@@ -11,8 +11,8 @@ export class CardComponent implements OnInit {
     constructor() { }
 
     @Input() cardModel: CardModel;
-    @Input() isCardNumberMasked;
-    @Input() isCardFlipped;
+    @Input() isCardNumberMasked: boolean;
+    @Input() isCardFlipped: boolean;
 
     currentCardNumberPlaceholder: string[];
     cardHolderName: string[];
@@ -21,57 +21,50 @@ export class CardComponent implements OnInit {
     defaultCardPlaceholder = '#### #### #### ####';
 
     ngOnInit() {
-        console.log(this.cardModel)
-        console.log(this.isCardNumberMasked)
         this.changePlaceholder();
-        console.log(this.currentCardNumberPlaceholder)
     }
 
+    getIsNumberMasked(index: number, cardPlaceHolderLetter: string): boolean {
+        return index > 4 && index < 14 && this.cardModel.cardNumber.length > index && cardPlaceHolderLetter.trim() !== '' && this.isCardNumberMasked;
+    }
 
-    changePlaceholder() {
+    private changePlaceholder(): void {
         const cardType = this.cardType();
         if (cardType === 'amex') {
-            this.currentCardNumberPlaceholder = this.amexCardPlaceholder.split('')
+            this.currentCardNumberPlaceholder = this.amexCardPlaceholder.split('');
         } else if (cardType === 'dinersclub') {
-            this.currentCardNumberPlaceholder = this.dinersCardPlaceholder.split('')
+            this.currentCardNumberPlaceholder = this.dinersCardPlaceholder.split('');
         } else {
-            this.currentCardNumberPlaceholder = this.defaultCardPlaceholder.split('')
+            this.currentCardNumberPlaceholder = this.defaultCardPlaceholder.split('');
         }
-        /*this.$nextTick(() => {
-            this.changeFocus()
-        })*/
     }
 
-    cardType() {
-        let number = this.cardModel.cardNumber
-        let re = new RegExp('^4')
-        if (number.match(re) != null) return 'visa'
+    private cardType(): string {
+        let number = this.cardModel.cardNumber;
+        let re = new RegExp('^4');
+        if (number.match(re) != null) return 'visa';
 
         re = new RegExp('^(34|37)')
-        if (number.match(re) != null) return 'amex'
+        if (number.match(re) != null) return 'amex';
 
         re = new RegExp('^5[1-5]')
-        if (number.match(re) != null) return 'mastercard'
+        if (number.match(re) != null) return 'mastercard';
 
         re = new RegExp('^6011')
-        if (number.match(re) != null) return 'discover'
+        if (number.match(re) != null) return 'discover';
 
         re = new RegExp('^62')
-        if (number.match(re) != null) return 'unionpay'
+        if (number.match(re) != null) return 'unionpay';
 
         re = new RegExp('^9792')
-        if (number.match(re) != null) return 'troy'
+        if (number.match(re) != null) return 'troy';
 
-        re = new RegExp('^3(?:0([0-5]|9)|[689]\\d?)\\d{0,11}')
-        if (number.match(re) != null) return 'dinersclub'
+        re = new RegExp('^3(?:0([0-5]|9)|[689]\\d?)\\d{0,11}');
+        if (number.match(re) != null) return 'dinersclub';
 
-        re = new RegExp('^35(2[89]|[3-8])')
-        if (number.match(re) != null) return 'jcb'
+        re = new RegExp('^35(2[89]|[3-8])');
+        if (number.match(re) != null) return 'jcb';
 
-        return '' // default type
-    }
-
-    getIsNumberMasked(index, n) {
-        return index > 4 && index < 14 && this.cardModel.cardNumber.length > index && n.trim() !== '' && this.isCardNumberMasked
+        return ''; // default type
     }
 }
