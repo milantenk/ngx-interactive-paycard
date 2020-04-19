df33cd50dfac83b21ee29171642b62d58b80a9e8
Milan Tenk <tenkmilan@gmail.com>
2020-04-19 11:39:33 +0200

    CardForm - Some cleaning done

--- projects/interactive-paycard-lib/src/lib/card-form/card-form.component.ts
+++ projects/interactive-paycard-lib/src/lib/card-form/card-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CardModel } from '../shared/card-model'
 
 @Component({
@@ -7,50 +7,36 @@ import { CardModel } from '../shared/card-model'
     styleUrls: ['./card-form.component.scss']
 })
 
-export class CardFormComponent implements OnInit {
+export class CardFormComponent {
 
-    cardModel: CardModel;
-
-    cardNumberId = 'cardNumberId';
-    cardNumberMaxLength = 19;
-    displayedCardNumber;
+    cardModel: CardModel = { cardNumber: '', cardName: '', expirationMonth: '', expirationYear: '', cvv: '' };
     isCardNumberMasked = true;
     isCvvFocused = false;
 
+    cardNumberMaxLength = 19;
+    minCardYear = new Date().getFullYear();
+    displayedCardNumber = this.cardModel.cardNumber; // The displayedCardNumber can be masked, the cardModel.cardNumber contains the real data
+
+    cardNumberId = 'cardNumberId';
     cardNameId = 'cardNameId';
-
     monthSelect = 'monthSelect';
-
-    minCardYear;
-
     yearSelectId = 'yearSelectId';
-
     cardCvvId = 'cardCvvId';
 
-    constructor() {
-        this.cardModel = { cardNumber: '', cardName: '', expirationMonth: '', expirationYear: '', cvv: '' }
-        this.displayedCardNumber = this.cardModel.cardNumber;
-    }
-
-    ngOnInit() {
-        this.minCardYear = new Date().getFullYear();
-    }
-
-    onCardNumberChange(event) {
-        let cardNumber = event.target.value;
-        let value = cardNumber.replace(/\D/g, '')
+    onCardNumberChange($event) {
+        let cardNumber: string = $event.target.value;
+        let value = cardNumber.replace(/\D/g, '');
         if ((/^3[47]\d{0,13}$/).test(value)) { // american express, 15 digits
-            cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{6})/, '$1 $2 ')
-            this.cardNumberMaxLength = 17
+            cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{6})/, '$1 $2 ');
+            this.cardNumberMaxLength = 17;
         } else if ((/^3(?:0[0-5]|[68]\d)\d{0,11}$/).test(value)) { // diner's club, 14 digits
-            cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{6})/, '$1 $2 ')
-            this.cardNumberMaxLength = 16
+            cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{6})/, '$1 $2 ');
+            this.cardNumberMaxLength = 16;
         } else if ((/^\d{0,16}$/).test(value)) { // regular cc number, 16 digits
-            cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ')
-            this.cardNumberMaxLength = 19
+            cardNumber = value.replace(/(\d{4})/, '$1 ').replace(/(\d{4}) (\d{4})/, '$1 $2 ').replace(/(\d{4}) (\d{4}) (\d{4})/, '$1 $2 $3 ');
+            this.cardNumberMaxLength = 19;
         }
         this.displayedCardNumber = cardNumber;
-        event.target.value = cardNumber;
         this.cardModel.cardNumber = cardNumber;
     }
 
@@ -60,7 +46,7 @@ export class CardFormComponent implements OnInit {
 
     onCardNumberBlur() {
         if (this.isCardNumberMasked) {
-            this.maskCardNumber()
+            this.maskCardNumber();
         }
     }
 
@@ -72,40 +58,49 @@ export class CardFormComponent implements OnInit {
         this.isCvvFocused = true;
     }
 
-    onCardNameKeyPress(event) {
-        return ((event.charCode >= 65 && event.charCode <= 90) || (event.charCode >= 97 && event.charCode <= 122) || (event.charCode == 32))
+    onCardNameKeyPress($event) {
+        return (($event.charCode >= 65 && $event.charCode <= 90) || ($event.charCode >= 97 && $event.charCode <= 122) || ($event.charCode == 32));
     }
 
-    maskCardNumber() {
-        this.cardModel.cardNumber = this.displayedCardNumber
-        let arr = this.displayedCardNumber.split('')
-        arr.forEach((element, index) => {
-            if (index > 4 && index < 14 && element.trim() !== '') {
-                arr[index] = '*'
-            }
-        })
-        this.displayedCardNumber = arr.join('')
-    }
-
-    unMaskCardNumber() {
-        this.displayedCardNumber = this.cardModel.cardNumber
-    }
-
-    toggleCardNumberMask() {
-        this.isCardNumberMasked = !this.isCardNumberMasked
+    onToggleCardNumberMask() {
+        this.isCardNumberMasked = !this.isCardNumberMasked;
         if (this.isCardNumberMasked) {
-            this.maskCardNumber()
+            this.maskCardNumber();
         } else {
-            this.unMaskCardNumber()
+            this.unMaskCardNumber();
+        }
+    }
+
+    onYearChange() {
+        if (this.cardModel.expirationYear === this.minCardYear.toString()) {
+            this.cardModel.expirationMonth = '';
         }
     }
 
     minCardMonth() {
-        if (this.cardModel.expirationYear === this.minCardYear) return new Date().getMonth() + 1
-        return 1
+        if (this.cardModel.expirationYear === this.minCardYear.toString()) {
+            return new Date().getMonth() + 1;
+        } else {
+            return 1;
+        }
     }
 
     generateMonthValue(n) {
-        return n < 10 ? `0${n}` : n
+        return n < 10 ? `0${n}` : n;
+    }
+
+    private maskCardNumber() {
+        this.cardModel.cardNumber = this.displayedCardNumber;
+        let arr = this.displayedCardNumber.split('');
+        arr.forEach((element, index) => {
+            if (index > 4 && index < 14 && element.trim() !== '') {
+                arr[index] = '*';
+            }
+        })
+        this.displayedCardNumber = arr.join('');
+    }
+
+    private unMaskCardNumber() {
+        this.displayedCardNumber = this.cardModel.cardNumber;
     }
 }
