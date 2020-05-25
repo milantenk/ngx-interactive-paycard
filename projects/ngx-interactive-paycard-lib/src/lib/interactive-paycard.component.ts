20bbf696945497cb0c26476b1c6e5853031efa8d
Milan Tenk <tenkmilan@gmail.com>
2020-05-25 20:37:19 +0200

    Card number delete fix

--- projects/ngx-interactive-paycard-lib/src/lib/interactive-paycard.component.ts
+++ projects/ngx-interactive-paycard-lib/src/lib/interactive-paycard.component.ts
@@ -32,6 +32,7 @@ export class InteractivePaycardComponent implements OnInit {
   cardCvvId = 'cardCvvId';
 
   focusedElement: FocusedElement;
+  cardNumberFormatArray: string[];
 
   ngOnInit() {
     if (new RegExp('[^# ]').test(this.cardNumberFormat)) {
@@ -43,33 +44,31 @@ export class InteractivePaycardComponent implements OnInit {
     if (this.cardNumberMask.length != this.cardNumberFormat.length) {
       throw new Error('The card number mask and the card number format must have the same length! Check the "cardNumberFormat" and the "cardNumberMask" input parameters!');
     }
+    this.cardNumberMaxLength = this.cardNumberFormat.length;
+    this.cardNumberFormatArray = this.cardNumberFormat.split('');
   }
 
   onCardNumberChange($event): void {
-    let cardNumber: string = $event.target.value;
-    this.cardNumberMaxLength = this.cardNumberFormat.length;
-
-    let newValues = [];
+    let processedCardNumber: string = $event.target.value;
+    let newValues:string[] = [];
     let letterRegex = new RegExp('[^0-9]');
-    cardNumber.split('').forEach((element, index) => {
-      if (this.cardNumberFormat[index] == ' ' && element != ' ' && !(letterRegex.test(element))) {
-        // if the typed number is at the border of a space, the space has to be added and the number itself
-        newValues.push(' ');
-        newValues.push(element);
-      } else if (this.cardNumberFormat[index] == ' ' && element == ' ') {
-        // if there is already a space in the number and in the placeholder, a space is needed
-        newValues.push(' ');
-      }
-      else if (!(letterRegex.test(element))) {
-        // only numbers are allowed
-        newValues.push(element);
+    let cardNumWithoutSpaceAsArray = processedCardNumber.replace(/ /g, '').split('');
+    this.cardNumberFormatArray.forEach((format) => {
+      if (cardNumWithoutSpaceAsArray.length > 0) {
+        if (format === '#') {
+          const character = cardNumWithoutSpaceAsArray.shift()
+          if (!(letterRegex.test(character))) {
+            newValues.push(character);
+          }
+        } else if (format === ' ') {
+          newValues.push(' ');
+        }
       }
     });
-    cardNumber = newValues.join('').trim();
-
-    this.displayedCardNumber = cardNumber;
-    this.cardModel.cardNumber = cardNumber;
-    $event.target.value = cardNumber; // The value in event has to be updated, otherwise the letter remains in the <input>
+    processedCardNumber = newValues.join('').trim();
+    this.displayedCardNumber = processedCardNumber;
+    this.cardModel.cardNumber = processedCardNumber;
+    $event.target.value = processedCardNumber; // The value in event has to be updated, otherwise the letter remains in the <input>
   }
 
   onCvvChange(event): void {
