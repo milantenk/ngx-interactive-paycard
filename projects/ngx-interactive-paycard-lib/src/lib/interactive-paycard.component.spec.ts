7ad44aba82d249492c05d3beb94b1359e625a9bc
Milan Tenk <tenkmilan@gmail.com>
2020-08-08 22:39:11 +0200

    Test fixed

--- projects/ngx-interactive-paycard-lib/src/lib/interactive-paycard.component.spec.ts
+++ projects/ngx-interactive-paycard-lib/src/lib/interactive-paycard.component.spec.ts
@@ -1,12 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { InteractivePaycardComponent } from './interactive-paycard.component';
-import { CardComponent } from './card/card.component';
-import { IfUndefinedChangesDirective } from './shared/if-undefined-changes.directive';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { InteractivePaycardComponent } from './interactive-paycard.component';
+import { CardComponent } from './card/card.component';
+import { IfUndefinedChangesDirective } from './shared/if-undefined-changes.directive';
+import { IfEveryChangesDirective } from './shared/if-every-changes.directive';
 
 describe('InteractivePaycardComponent', () => {
   let component: InteractivePaycardComponent;
@@ -14,7 +15,7 @@ describe('InteractivePaycardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InteractivePaycardComponent, CardComponent, IfUndefinedChangesDirective ],
+      declarations: [ InteractivePaycardComponent, CardComponent, IfUndefinedChangesDirective, IfEveryChangesDirective ],
       imports: [
         FormsModule,
         CommonModule,
