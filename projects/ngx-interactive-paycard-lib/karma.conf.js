1d42757f03a30bbe6a6301914de8ccdcbef36031
Milan Tenk <tenkmilan@gmail.com>
2020-08-09 10:23:24 +0200

    Test travis

--- projects/ngx-interactive-paycard-lib/karma.conf.js
+++ projects/ngx-interactive-paycard-lib/karma.conf.js
@@ -21,6 +21,12 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
