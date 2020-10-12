2378f1b68150e21353f2c47a07ee43d8aa907feb
Alexandre Insua <alexandreinsua@gmail.com>
2020-10-12 11:04:04 +0200

    added some e2e test

--- projects/ngx-interactive-paycard-demo/e2e/src/app.po.ts
+++ projects/ngx-interactive-paycard-demo/e2e/src/app.po.ts
@@ -5,7 +5,21 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
+  getInputElementsNumber() {
+    return element.all(by.css('input'));
+  }
+
+  getSelectElementsNumber() {
+    return element.all(by.css('select'));
+  }
+
   getButtonText() {
     return element(by.buttonText('Submit')).getText() as Promise<string>;
   }
+
+  getElementById(id: string) {
+    return element(by.id(id));
+  }
 }
+
+
