81b49a128ed11752c3a230718709f4e1a1b171bd
Vithor <vithort@gmail.com>
2020-12-07 08:19:19 -0300

    Update: Remove function to Verify brand
    Update: Remove function to Verify brand

--- projects/ngx-interactive-paycard-demo/src/app/app.component.ts
+++ projects/ngx-interactive-paycard-demo/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CardLabel, FormLabel } from 'ngx-interactive-paycard-lib';
-import { creditCards } from '../assets/credit-card';
 
 @Component({
   selector: 'app-root',
@@ -29,8 +28,6 @@ export class AppComponent {
     cvv: 'CVV',
     submitButton: 'Enviar',
   };
-  creditCards = creditCards;
-  brand = './assets/logo.png';
 
   onSubmitEvent($event) {
     console.log($event);
@@ -44,17 +41,5 @@ export class AppComponent {
   showChangesCardNumber($event) {
     // any changes on card number
     console.log($event);
-    this.verifyBrand($event);
-  }
-
-  // with this function, you can verify the brand of the card (file: assets/credit-card.ts)
-  verifyBrand(cardNumber) {
-    const found = this.creditCards.filter( item => item.startsWith.find( el => cardNumber.substring(0, el.length) === el));
-    this.brand = './assets/logo.png';
-    if (found.length > 0) {
-      const foundOne = found[0];
-      this.brand = foundOne.logo;
-      console.log(foundOne.name);
-    }
   }
 }
